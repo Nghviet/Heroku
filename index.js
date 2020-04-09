aceaef3af90e3a3961aad1e294642fee209aa4f7
Nguyen Hoang Viet <NgHViet@Nguyens-MacBook-Pro.local>
2020-04-09 13:49:04 +0700

    inti

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/',(req,res) => {
+    res.send("Listening on " + port);
+})
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+app.listen(port);
